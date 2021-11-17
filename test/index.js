0e48001f96690eb03b2d80b03a7521366c18867d
Temitope3665 <aroyewontope@gmail.com>
2021-11-18 00:43:31 +0100

    register user test passed

--- test/index.js
+++ test/index.js
@@ -2,7 +2,6 @@ const {expect} = require('chai')
 const request = require('supertest')
 const app = require('../src/index')
 
-
 describe('book', () => {
     it('baseroute', (done) => {
         request(app)
@@ -17,30 +16,49 @@ describe('book', () => {
 
     it('registerUser', (done) => {
         request(app)
-        .post('/register-user')
+        .post(`/register-user`)
         .send({
-            firstName: 'Olajide',
-            lastName: 'Michael',
+            firstName: 'Lewis',
+            lastName: 'Gordon',
             role: 'user',
-            email: 'mich@gmail.com',
-            password: 'mich'
+            email: 'lew@gmail.com',
+            password: 'lewis'
         })
         .expect(201)
         .end((err, res) => {
-            console.log(res.body)
-            // blogId = res.body.data.id
             expect(res.body.message).to.equal('User created successfully')
             expect(res.body.code).to.equal(201)
             expect(res.body.status).to.equal('success')
-            // expect(res.body.data).to.be.an('object')
-            // expect(res.body.data).to.have.property('id')
-            // expect(res.body.data).to.have.property('email')
-            // expect(res.body.data).to.have.property('password')
-            // expect(res.body.data).to.have.property('firstName')
-            // expect(res.body.data).to.have.property('lastName')
-            // expect(res.body.data).to.have.property('role')
+            expect(res.body.data).to.be.an('object')
+            expect(res.body.data).to.have.property('id')
+            expect(res.body.data).to.have.property('email')
+            expect(res.body.data).to.have.property('password')
+            expect(res.body.data).to.have.property('firstName')
+            expect(res.body.data).to.have.property('lastName')
+            expect(res.body.data).to.have.property('role')
             done()
         })
     })
+
+    // it('loginUser', (done) => {
+    //     request(app)
+    //     .post('/login-user')
+    //     .send({
+    //         email: 'lara@gmail.com',
+    //         password: 'lara'
+    //     })
+    //     .expect(201)
+    //     .end((err, res) => {
+    //         console.log(res.body)
+    //         expect(res.body.message).to.equal('User logged in successfully')
+    //         expect(res.body.code).to.equal(201)
+    //         expect(res.body.status).to.equal('success')
+    //         // expect(res.body.data).to.be.an('object')
+    //         // expect(res.body.data).to.have.property('id')
+    //         // expect(res.body.data).to.have.property('email')
+    //         // expect(res.body.data).to.have.property('password')
+    //         done()
+    //     })
+    // })
 
 })
